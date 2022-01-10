be15660c32cc362708477fb15146236c82461546
Ch1keen <40013212+Ch1keen@users.noreply.github.com>
2022-01-10 16:03:11 +0000

    fix: Finally I know how to use mach-nix a bit

--- nix/pwnable/shell.nix
+++ nix/pwnable/shell.nix
@@ -1,63 +1,79 @@
 { pkgs ? import <nixpkgs> {}
 }:
-  pkgs.mkShell {
-    name="pwnable";
+  let
+    mach-nix = import (builtins.fetchGit {
+      url = "https://github.com/DavHau/mach-nix/";
+      ref = "refs/tags/3.3.0";
+    }) {};
+    angr = mach-nix.mkPython {
+        python = "python39Full";
+        requirements = ''
+          angr
+        '';
+      };
+    miasm = mach-nix.mkPython {
+        python = "python39Full";
+        requirements = ''
+          miasm
+        '';
+      };
+    qiling = mach-nix.mkPython {
+        python = "python39Full";
+        requirements = ''
+          qiling
+        '';
+      };
 
-    buildInputs = [
-      # Locales
-      pkgs.glibcLocales
+  in
+    pkgs.mkShell {
+      name="pwnable";
 
-      # Language for writing scripts
-      pkgs.python39Full
-      pkgs.python39Packages.pylint
-      pkgs.ruby_3_0
-      pkgs.rubyPackages_3_0.pry
+      buildInputs = [
+        # Meta
+        pkgs.glibcLocales
+        pkgs.cmake
 
-      # NeoVim & tmux rules
-      pkgs.neovim
-      pkgs.tmux
-      pkgs.fish
+        # Language for writing scripts
+        pkgs.python39Full
+        pkgs.python39Packages.pylint
+        pkgs.ruby_3_0
+        pkgs.rubyPackages_3_0.pry
 
-      # Pwntools, itself
-      pkgs.python39Packages.pwntools
+        # NeoVim & tmux rules
+        pkgs.neovim
+        pkgs.tmux
+        pkgs.fish
 
-      # Angr
-      (let
-        mach-nix = import (builtins.fetchGit {
-          url = "https://github.com/DavHau/mach-nix/";
-          ref = "refs/tags/3.3.0";
-        }) {};
-      in
-        mach-nix.mkPython {
-          requirements = ''
-            qiling
-            miasm
-	      '';
-	      }
-      )
+        # Pwntools, itself
+        pkgs.python39Packages.pwntools
 
-      # Emulation
-      pkgs.unicorn
-      pkgs.python3Packages.unicorn
-      pkgs.qemu_full
+        # Angr
+        angr
+        miasm
+        
+        # Emulation
+        pkgs.unicorn
+        pkgs.python3Packages.unicorn
+        pkgs.qemu_full
+        qiling
 
-      # Debugging or Binary analysis
-      pkgs.radare2
-      pkgs.ghidra-bin
-      pkgs.gdb
+        # Debugging or Binary analysis
+        pkgs.radare2
+        pkgs.ghidra-bin
+        pkgs.pwndbg
 
-      # Default Networking
-      pkgs.openssh
-      pkgs.netcat
+        # Default Networking
+        pkgs.openssh
+        pkgs.netcat
 
-      pkgs.tree
-    ];
-    shellHook = ''
-      r2pm update
-      r2pm -i r2ghidra
-      r2pm -ci r2dec
-      export SHELL=$(which fish)
+        pkgs.tree
+      ];
+      shellHook = ''
+        r2pm update
+        r2pm -i r2ghidra
+        r2pm -ci r2dec
+        export SHELL=$(which fish)
 
-      tmux -2
-    '';
-  }
+        tmux -2
+      '';
+    }
