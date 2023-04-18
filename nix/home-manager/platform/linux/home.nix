8fa58ce5505e49fcb5c137b0687a33f13a26a66b
Ch1keen <gihoong7@gmail.com>
2023-04-18 15:45:09 +0900

    refactor: Saparate neovim & tmux config

--- nix/home-manager/platform/linux/home.nix
+++ nix/home-manager/platform/linux/home.nix
@@ -89,6 +89,11 @@ in
     pkgs.htop
   ];
 
+  imports = [
+    ../../src/neovim.nix
+    ../../src/tmux.nix
+  ];
+
   # Korean Language
   home.sessionVariables = {
     GTK_IM_MODULE = "kime";
@@ -125,127 +130,6 @@ in
   # qutebrowser
   programs.qutebrowser.enable = true;
 
-  # Number of editors: neovim and emacs
-  programs.neovim.enable = true;
-  programs.neovim.coc.enable = true;
-  programs.neovim.coc.settings = {
-    # Python: pyright
-    "pyright.enable" = true;
-    "python.linting.enabled" = true;
-    "python.linting.pylintEnabled" = true;
-
-    # Ruby: rubocop & solargraph
-    "solargraph.diagnostics" = true;
-  };
-  programs.neovim.extraConfig = ''
-    set nu
-    set autoindent
-    set expandtab
-    set smartindent
-    set cindent
-
-    autocmd BufWritePre * :%s/\s\+$//e
-
-    " CoC Settings
-    set signcolumn=yes
-    set updatetime=300
-
-    inoremap <silent><expr> <TAB>
-      \ coc#pum#visible() ? coc#pum#next(1) :
-      \ CheckBackspace() ? "\<Tab>" :
-      \ coc#refresh()
-    inoremap <expr><S-TAB> coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"
-
-    " Make <CR> to accept selected completion item or notify coc.nvim to format
-    " <C-g>u breaks current undo, please make your own choice.
-    inoremap <silent><expr> <CR> coc#pum#visible() ? coc#pum#confirm()
-                              \: "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"
-
-    function! CheckBackspace() abort
-      let col = col('.') - 1
-      return !col || getline('.')[col - 1]  =~# '\s'
-    endfunction
-
-    " Use <c-space> to trigger completion.
-    if has('nvim')
-      inoremap <silent><expr> <c-space> coc#refresh()
-    else
-      inoremap <silent><expr> <c-@> coc#refresh()
-    endif
-  '';
-  programs.neovim.plugins = with pkgs; [
-    {
-      plugin = vimPlugins.neon;
-      config = "colorscheme neon";
-    }
-
-    vimPlugins.vim-airline
-    vimPlugins.bufferline-nvim
-    vimPlugins.nvim-ts-rainbow
-    vimPlugins.vim-nix
-
-    {
-      plugin = vimPlugins.nvim-lint;
-      type = "lua";
-      config = ''
-        require('lint').linters_by_ft = {
-          nix = {'nix',},
-          c = {'clangtidy'},
-          cpp = {'clangtidy'}
-        }
-
-        vim.api.nvim_create_autocmd({ "InsertLeave" }, {
-          callback = function()
-            require("lint").try_lint()
-          end,
-        })
-      '';
-    }
-
-    {
-      plugin = vimPlugins.nvim-tree-lua;
-      type = "lua";
-      config = ''
-        -- disable netrw at the very start of your init.lua (strongly advised)
-        vim.g.loaded_netrw = 1
-        vim.g.loaded_netrwPlugin = 1
-
-        require("nvim-tree").setup()
-      '';
-    }
-
-    {
-      plugin = vimPlugins.gitsigns-nvim;
-      type = "lua";
-      config = "require('gitsigns').setup()";
-    }
-
-    # TreeSitter
-    (pkgs.vimPlugins.nvim-treesitter.withPlugins (plugins: [
-      plugins.tree-sitter-nix
-      plugins.tree-sitter-ruby
-      plugins.tree-sitter-python
-      plugins.tree-sitter-javascript
-      plugins.tree-sitter-typescript
-      plugins.tree-sitter-tsx
-      plugins.tree-sitter-c
-      plugins.tree-sitter-cpp
-      plugins.tree-sitter-haskell
-      plugins.tree-sitter-ocaml
-      plugins.tree-sitter-dockerfile
-      plugins.tree-sitter-yaml
-    ]))
-
-    # CoC
-    vimPlugins.coc-json
-    vimPlugins.coc-clangd
-    vimPlugins.coc-solargraph
-    vimPlugins.coc-pyright
-    vimPlugins.coc-eslint
-    vimPlugins.coc-prettier
-    vimPlugins.coc-rust-analyzer
-  ];
-
   # ZSH Shell
   programs.zsh = {
     enable = true;
@@ -271,25 +155,6 @@ in
 
   # Alacritty
   programs.alacritty.enable = true;
-
-  # Tmux
-  programs.tmux = {
-    enable = true;
-    shell = "${pkgs.zsh}/bin/zsh";
-    terminal = "screen-256color";
-    plugins = with pkgs.tmuxPlugins; [
-      sidebar
-      {
-	plugin = dracula;
-	extraConfig = ''
-	  set -g @dracula-show-battery true
-	  set -g @dracula-show-powerline true
-          set -g @dracula-show-fahrenheit false
-	  set -g @dracula-refresh-rate 10
-	'';
-      }
-    ];
-  };
 
   # rbenv
   programs.rbenv = {
